d36ef32e3f31be86cd352ce91aa8a95383956f35
@hexlet/cli <support@hexlet.io>
2023-07-28 23:54:34 +0300

    submit ruby-basics-ru/testing

--- ruby-basics-ru/testing/test/stack_test.rb
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -5,36 +5,39 @@ require_relative '../lib/stack'
 
 class StackTest < Minitest::Test
   # BEGIN
-  def setup
-    @st = Stack.new
-    @data = '51515'
-  end
-
   def test_add_item
-    @st.push! @date
-    assert { @st.size == 1 }
-    assert { @data == @st.to_a.first }
+    st = Stack.new
+    data = '51515'
+    st.push! data
+    assert { st.size == 1 }
+    assert { data == st.to_a.first }
   end
 
   def test_remove_item
-    @st.push! @data
-    item = @st.pop!
-    assert { @data == item }
-    assert { @st.size.empty? }
+    st = Stack.new
+    data = '51515'
+    st.push! data
+    item = st.pop!
+    assert { data == item }
+    assert { st.size == 0 }
   end
 
   def test_clear_stack
-    @st.push! @data
-    @st.clear!
-    assert { @st.size.empty? }
-    assert { @st.to_a == [] }
+    st = Stack.new
+    data = '51515'
+    st.push! data
+    st.clear!
+    assert { st.size == 0 }
+    assert { st.to_a == [] }
   end
 
   def test_check_empty_stack
-    assert { @st.empty? }
-    @st.push! @data
-    @st.clear!
-    assert { @st.empty? }
+    st = Stack.new
+    data = '51515'
+    st.push! data
+    refute { st.empty? }
+    st.clear!
+    assert { st.empty? }
   end
   # END
 end
